567a835ea9d8f844cc45eaf0eeea3634c222e2f4
hauntarl <sameer.mungole@gmail.com>
2021-06-30 01:30:34 +0530

    create table for phone numbers

--- db/db.go
+++ db/db.go
@@ -2,9 +2,8 @@ package db
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 	"os"
-	"path/filepath"
 
 	_ "modernc.org/sqlite"
 )
@@ -12,18 +11,21 @@ import (
 // Open creates a new sqlite database file,
 // returns the pointer to database.
 func Open(name string) (*sql.DB, error) {
-	log.Printf("Creating Database: %v", name)
 	f, err := os.Create(name)
 	if err != nil {
 		return nil, err
 	}
 	f.Close()
 
-	abs, err := filepath.Abs(name)
-	if err != nil {
-		abs = "./" + name
-	}
-	log.Printf("Database Location: %v", abs)
-
 	return sql.Open("sqlite", name)
+}
+
+func CreateTable(dp *sql.DB, name string) error {
+	query := fmt.Sprintf(`
+	CREATE TABLE IF NOT EXISTS %s (
+		id SERIAL,
+		value VARCHAR(255)
+	)`, name)
+	_, err := dp.Exec(query)
+	return err
 }
--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"log"
+	"path/filepath"
 
 	"github.com/hauntarl/phone-number/db"
 )
 
-const database = "phone-number.db"
+const (
+	dbName    = "phone-numbers.db"
+	tableName = "phone_numbers"
+)
 
 func main() {
-	db, err := db.Open(database)
+	log.Printf("Creating Database: %v", dbName)
+	dp, err := db.Open(dbName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer dp.Close()
+	abs, _ := filepath.Abs(dbName)
+	log.Printf("Database Location: %v", abs)
+
+	if err := db.CreateTable(dp, tableName); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Created Table: %v", tableName)
 }
